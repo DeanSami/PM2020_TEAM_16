09a9fd3797f558b63af6be31a5a659446aa596dd
ofektalker <ofekt7144@gmail.com>
2020-04-05 21:16:12 +0300

    added token to UserSession

--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -1,18 +1,18 @@
-var express = require('express')
-const db = require('../db-connect')
-var router = express.Router()
-
+var express = require('express');
+const db = require('../db-connect');
+var router = express.Router();
+var hat = require('hat');
 router.use(function adminLog (req, res, next) {
-    console.log('<LOG> -', new Date().toUTCString())
-    next()
-})
+    console.log('<LOG> -', new Date().toUTCString());
+    next();
+});
 
 router.use(function adminLog (req, res, next) {
     if (req.originalUrl != '/admin/login') {
-        console.log(JSON.stringify(req.headers)['accept'])
+        console.log(JSON.stringify(req.headers)['accept']);
     }
-    next()
-})
+    next();
+});
 
 router.post('/dog_parks/add', function (req, res) {
     //......
@@ -22,22 +22,25 @@ router.post('/dog_parks/add', function (req, res) {
     // filter results
     // return information
     //......
-    res.json({})
-})
+    res.json({});
+});
 
 router.post('/login', function (req, res) {
-    console.log('<LOG> - Admin Login')
-    const phone = req.body.phone
-    const password = req.body.pass
+    console.log('<LOG> - Admin Login');
+    const phone = req.body.phone;
+    const password = req.body.pass;
     // encode pass
     db.query('SELECT * FROM users WHERE phone = ? AND password = ?', [phone, password], function (err, result) {
-        if (err) console.error(err)
+        if (err) console.error(err);
         if (result.length > 0) {
-            delete result[0].password
+            delete result[0].password;
             // create token for user
+            var token = hat();
             // insert to db to user_sessions
+            console.log("token:", token);
             res.json({
                 message: 'Admin Login Success',
+                token: token,
                 user: result[0]
             })
         } else {
@@ -46,8 +49,8 @@ router.post('/login', function (req, res) {
             })
         }
     })
-})
+});
 
-module.exports = router
+module.exports = router;
 
 // INSERT INTO `users` (`id`, `name`, `user_type`, `email`, `phone`, `password`, `avatar`, `deleted`, `created_at`, `update_at`) VALUES (NULL, 'Admin', '0', 'admin@doggiehunt.co.il', '0666', 'admin', '', '', CURRENT_TIMESTAMP, '');
