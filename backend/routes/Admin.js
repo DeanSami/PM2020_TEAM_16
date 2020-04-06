20b7766b9574b3498a5d587781c817deb624da57
DeanzOo <deanabutbul@gmail.com>
2020-04-06 15:01:33 +0300

    added missing check from adding new dog park

--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -40,42 +40,48 @@ else {
 router.post('/dog_parks/add', function (req, res) {
     console.log('<LOG> - POST /admin/dog_parks/add')
 
-    const {
-        type,
-        name, 
-        SHAPE_Leng, 
-        SHAPE_Area, 
-        street,
-        house_number,
-        neighborhood,
-        operator,
-        handicapped,
-        condition
-    } = req.body.user_input;
-
-    if (type !== Number || name !== String || SHAPE_Leng !== String || SHAPE_Area !== String ||
-        house_number !== String || neighborhood !== String || operator !== String || handicapped !== Boolean || condition !== Boolean)
-        console.error("typeof error");
-    var values = {type:type, name:name, SHAPE_Leng:SHAPE_Leng, SHAPE_Area:SHAPE_Area, house_number:house_number, neighborhood:neighborhood, operator:operator, handicapped:handicapped, condition:condition};
-    if (street != NULL)
-        values.street = street;
+    if (!req.body.user_input) {
+        console.log('<LOG> - POST /dog_parks/add - Wrong Payload Format')
+        res.statusCode = 401
+        res.json(globals.messages.failure)
+    } else {
+        const {
+            type,
+            name, 
+            SHAPE_Leng, 
+            SHAPE_Area, 
+            street,
+            house_number,
+            neighborhood,
+            operator,
+            handicapped,
+            condition
+        } = req.body.user_input;
     
-     db.query('INSERT INTO places SET ?', values, function (err, result) {
-         if (err) {
-                console.log('<LOG> - POST /admin/dog_parks/add - ERROR')
-                console.error(err)
-                res.json(globals.messages.failure)
-            } else {
-               console.log('<LOG> - POST /admin/dog_parks/add SUCCESS')
-                res.json({
-                 status: true
-                })
-        }
-    })
+        if (type !== Number || name !== String || SHAPE_Leng !== String || SHAPE_Area !== String ||
+            house_number !== String || neighborhood !== String || operator !== String || handicapped !== Boolean || condition !== Boolean)
+            console.error("typeof error");
+        var values = {type:type, name:name, SHAPE_Leng:SHAPE_Leng, SHAPE_Area:SHAPE_Area, house_number:house_number, neighborhood:neighborhood, operator:operator, handicapped:handicapped, condition:condition};
+        if (street != NULL)
+            values.street = street;
+        
+         db.query('INSERT INTO places SET ?', values, function (err, result) {
+             if (err) {
+                    console.log('<LOG> - POST /admin/dog_parks/add - ERROR')
+                    console.error(err)
+                    res.json(globals.messages.failure)
+                } else {
+                   console.log('<LOG> - POST /admin/dog_parks/add SUCCESS')
+                    res.json({
+                     status: true
+                    })
+            }
+        })
+    }
 
 });
 
-router.get('/dog_parks/get' , function(req,res){
+router.get('/dog_parks/get' , function(req, res) {
     console.log('<LOG> - Admin GET Park Dog');
     //if the clint want specific park dog
     if (req.body.id)
@@ -105,7 +111,6 @@ router.get('/dog_parks/get' , function(req,res){
                 console.log(result);
                 res.json({
                     status: true,
-
                     place: result
                 })
             }
