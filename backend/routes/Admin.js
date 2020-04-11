f619ebef04414f2df0a166e63beccafc6baf5096
ofektalker <ofekt7144@gmail.com>
2020-04-11 13:14:38 +0300

    endpoint for interest points

--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -6,43 +6,42 @@ const hat = require('hat')
 const bcrypt = require('bcrypt')
 
 router.use(function isAdmin (req, res, next) {
-    if (req.originalUrl == '/admin/login' || req.originalUrl == '/admin/register') next()
+    if (req.originalUrl == '/admin/login' || req.originalUrl == '/admin/register') next();
     else {
     console.log('<LOG> - POST /admin/* - Middleware')
-        const incoming_token = JSON.parse(JSON.stringify(req.headers))['x-auth']
+        const incoming_token = JSON.parse(JSON.stringify(req.headers))['x-auth'];
         if (incoming_token) {
             db.query('SELECT * FROM user_sessions, users WHERE user_sessions.user_id = users.id AND user_sessions.session = ? AND user_type = ?', [incoming_token, globals.user_types.admin], function (err, result) {
                 if (err) {
-                    console.log('<LOG> - POST /admin/* - ERROR')
-                    console.error(err)
-                    res.statusCode = 400
+                    console.log('<LOG> - POST /admin/* - ERROR');
+                    console.error(err);
+                    res.statusCode = 400;
                     res.json(globals.messages.failure)
                 }
                 else if (result.length > 0) {
-                    console.log('<LOG> - POST /admin/* - SUCCESS')
+                    console.log('<LOG> - POST /admin/* - SUCCESS');
                     next()
                 } else {
-                    console.log('<LOG> - POST /admin/* - Unauthorized Access Attempt')
-                    res.statusCode = 401
+                    console.log('<LOG> - POST /admin/* - Unauthorized Access Attempt');
+                    res.statusCode = 401;
                     res.json(globals.messages.failure)
                 }
             })
         } else {
-            console.log('<LOG> - POST /admin/* - Missing Credentials')
-            res.statusCode = 401
+            console.log('<LOG> - POST /admin/* - Missing Credentials');
+            res.statusCode = 401;
             res.json(globals.messages.failure)
         }
     }
 });
 
 router.use(globals.log_func);
-
+//ADD PARK REQUEST
 router.post('/dog_parks/add', function (req, res) {
-    console.log('<LOG> - POST /admin/dog_parks/add - Invoke')
+    console.log('<LOG> - POST /admin/dog_parks/add - Invoke');
 
     if (!req.body.user_input) {
-        console.log('<LOG> - POST /dog_parks/add - Wrong Payload Format')
-        res.statusCode = 400
+        console.log('<LOG> - POST /dog_parks/add - Wrong Payload Format');
         res.json(globals.messages.failure)
     } else {
         const {
@@ -66,8 +65,8 @@ router.post('/dog_parks/add', function (req, res) {
             || handicapped == undefined
             || condition == undefined)
             {
-                console.log('<LOG> - POST /dog_parks/add - At least 1 field is missing')
-                res.statusCode = 400
+                console.log('<LOG> - POST /dog_parks/add - At least 1 field is missing');
+                res.statusCode = 400;
                 res.json(globals.messages.failure)
             }
         else if (typeof(name) !== 'string'
@@ -79,8 +78,8 @@ router.post('/dog_parks/add', function (req, res) {
             || typeof(handicapped) !== 'boolean'
             || typeof(condition) !== 'number')
         {
-            console.log('<LOG> - POST /dog_parks/add - Error with type of at least 1 input field')
-            res.statusCode = 400
+            console.log('<LOG> - POST /dog_parks/add - Error with type of at least 1 input field');
+            res.statusCode = 400;
             res.json(globals.messages.failure)
         } else {
             var values = {type: 0, name:name, SHAPE_Leng:SHAPE_Leng, SHAPE_Area:SHAPE_Area, house_number:house_number,neighborhood:neighborhood, operator:operator, handicapped:handicapped, condition:condition};
@@ -95,12 +94,12 @@ router.post('/dog_parks/add', function (req, res) {
                 } else {
                     db.query('SELECT * FROM places WHERE id = (?)', [insert_dog_park_result.insertId], function (err, select_dog_park_result) {
                         if (err) {
-                            console.log('<LOG> - POST /admin/dog_parks/add - ERROR')
-                            console.error(err)
-                            res.statusCode = 400
+                            console.log('<LOG> - POST /admin/dog_parks/add - ERROR');
+                            console.error(err);
+                            res.statusCode = 400;
                             res.json(globals.messages.failure)
                         } else {
-                            console.log('<LOG> - POST /admin/dog_parks/add - SUCCESS')
+                            console.log('<LOG> - POST /admin/dog_parks/add - SUCCESS');
                             res.json({
                                 status: true,
                                 places: select_dog_park_result
@@ -113,32 +112,14 @@ router.post('/dog_parks/add', function (req, res) {
     }
 
 });
-
+//GET PARK REQUEST
 router.get('/dog_parks/get' , function(req, res) {
     console.log('<LOG> - GET /admin/dog_parks/get - Invoke');
     //if the clint want specific park dog
-    if (req.body.id)
-    {
+    if (req.body.id) {
         var temp_id = req.body.id;
-        db.query('SELECT * FROM places WHERE id = ?',[temp_id] , function (err,result) {
-            if(err) {
-                console.log('<LOG> - GET /admin/dog_parks/get - ERROR');
-                console.error(err);
-                res.statusCode = 400;
-                res.json(globals.messages.failure);
-            } else {
-                console.log('<LOG> - GET /admin/dog_parks/get - SUCCESS');
-                res.json({
-                    status:true,
-                    places:result
-                })
-            }
-        })
-    }
-    //if the clint want all the park dog that at the db
-    else{
-        //todo enum to set Places.dogPark and remove 0
-        db.query('SELECT * FROM places WHERE deleted = 0 AND type = 0', function (err, result) {
+        var temp_type = 0;
+        db.query('SELECT * FROM places WHERE id = ? AND type = ?', [temp_id, temp_type], function (err, result) {
             if (err) {
                 console.log('<LOG> - GET /admin/dog_parks/get - ERROR');
                 console.error(err);
@@ -149,20 +130,36 @@ router.get('/dog_parks/get' , function(req, res) {
                 res.json({
                     status: true,
                     places: result
-                })
-            }
-        })
-    }
-
-
-});
-//delete park
+                    })
+                }
+            })
+        }
+        //if the clint want all the park dog that at the db
+        else {
+            //todo enum to set Places.dogPark and remove 0
+            db.query('SELECT * FROM places WHERE deleted = 0 AND type = 0', function (err, result) {
+                if (err) {
+                    console.log('<LOG> - GET /admin/dog_parks/get - ERROR');
+                    console.error(err);
+                    res.statusCode = 400;
+                    res.json(globals.messages.failure);
+                } else {
+                    console.log('<LOG> - GET /admin/dog_parks/get - SUCCESS');
+                    res.json({
+                        status: true,
+                        places: result
+                    })
+                }
+            })
+        }
+    });
+//DELETE PARK REQUEST
 router.post('/dog_parks/delete',function (req,res) {
     console.log('<LOG> - POST /dog_parks/delete - Invoke');
     if(req.body.id)
     {
         var temp_id = req.body.id;
-        db.query('UPDATE places SET deleted = 1 WHERE id = ? ', [2],function (err,result) {
+        db.query('UPDATE places SET deleted = 1 WHERE id = ? ', [req.body.id],function (err,result) {
             console.log(result);
             if (err) {
                 console.log('<LOG> - POST /admin/DELETE - ERROR');
@@ -186,7 +183,7 @@ router.post('/dog_parks/delete',function (req,res) {
         res.json(globals.messages.failure);
     }
 });
-
+//REGISTER REQUEST
 router.post('/register', function(req, res) {
     bcrypt.hash(req.body.pass, 10, function (err, hash) {
         const values = {phone: req.body.phone, password: hash, user_type: 0}
@@ -194,8 +191,8 @@ router.post('/register', function(req, res) {
             if (!err) res.json(globals.messages.success)
         })
     })
-})
-
+});
+//LOGIN REQUEST
 router.post('/login', function (req, res) {
     console.log('<LOG> - POST /admin/login - Invoke');
     const phone = req.body.phone;
@@ -210,12 +207,12 @@ router.post('/login', function (req, res) {
                 bcrypt.compare(password, phone_query_result[0].password, function (err, pass_compare) {
                     if (err) {
                         console.log('<LOG> - POST /admin/login - ERROR');
-                        console.error(err)
+                        console.error(err);
                         res.json(globals.messages.failure)
                     } else {
                         if (!pass_compare) {
                             console.log('<LOG> - POST /admin/login - Wrong Credentials pass');
-                            res.statusCode = 401
+                            res.statusCode = 401;
                             res.json(globals.messages.failure)
                         } else {
                             delete phone_query_result[0].password;
@@ -240,7 +237,7 @@ router.post('/login', function (req, res) {
                 })
             } else {
                 console.log('<LOG> - POST /admin/login - Wrong Credentials');
-                res.statusCode = 401
+                res.statusCode = 401;
                 res.json(globals.messages.failure)
             }
         }
@@ -254,11 +251,11 @@ router.get('/login', function (req, res) {
         db.query('SELECT * FROM user_sessions, users WHERE user_sessions.user_id = users.id AND user_sessions.session = ?', [incoming_token], function(err, result) {
             if (err) {
                 console.log('<LOG> - GET /admin/login - ERROR');
-                console.error(err)
+                console.error(err);
                 res.json(globals.messages.failure)
             } else {
                 if (result.length > 0) {
-                    delete result[0].password
+                    delete result[0].password;
                     console.log('<LOG> - GET /admin/login - SUCCESS');
                     res.json({
                         status: true,
@@ -266,16 +263,51 @@ router.get('/login', function (req, res) {
                     })
                 } else {
                     console.log('<LOG> - GET /admin/login - Unauthorized Credentials');
-                    res.statusCode = 401
+                    res.statusCode = 401;
                     res.json(globals.messages.failure)
                 }
             }
         })
     } else {
         console.log('<LOG> - GET /admin/login - Credentials Missing');
-        res.statusCode = 401
+        res.statusCode = 401;
         res.json(globals.messages.failure)
     }
 });
+//GET REQUEST INTEREST POINT
+router.get('/interestpoint',function(req,res){
+    console.log('<LOG> - GET /interestpoint - Invoke');
+    if(req.body.id)
+    {
+        var temp_id = req.body.id;
+        var temp_type = 0;
+        db.query('SELECT * FROM places WHERE id = ? AND type != ?',[temp_id,temp_type],function(err,result){
+           if(err){
+               console.log('<LOG> - GET /interest point - ERROR');
+               console.error(err);
+               res.json(globals.messages.failure)
+           } else {
+               if(result.length>0){
+                   console.log('<LOG> - GET /interest point - SUCCESS');
+                   res.json({
+                       status:true,
+                       place: result[0]
+                   })
+               } else {
+                   console.log('<LOG> - GET /interest point - Unauthorized Credentials');
+                   res.statusCode = 401;
+                   res.json(globals.messages.failure)
+               }
 
+           }
+        });
+    }
+
+});
+// export enum PlacesType {
+//     Dog_garden = 0,
+//         Historic_Parks = 1,
+//         Cafewithdog = 2,
+//         NationalParks = 3,
+// }
 module.exports = router;
