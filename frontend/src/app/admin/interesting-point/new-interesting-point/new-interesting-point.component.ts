6bdcab6ac5cd7ea00e4ded8c4755c363c08717a6
deconltd <dorsh3@users.noreply.github.com>
2020-04-12 17:09:02 +0300

    heb

---
+++ frontend/src/app/admin/interesting-point/new-interesting-point/new-interesting-point.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-interesting-point',
+  templateUrl: './new-interesting-point.component.html',
+  styleUrls: ['./new-interesting-point.component.scss']
+})
+export class NewInterestingPointComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
