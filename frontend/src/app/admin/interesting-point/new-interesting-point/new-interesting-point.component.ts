229ec96b66f0d80a2fe75fa05f3e5988edfd6ec2
eden ohana <edenoh2@ac.sce.ac.il>
2020-04-14 01:12:13 +0300

    new point ts

--- frontend/src/app/admin/interesting-point/new-interesting-point/new-interesting-point.component.ts
+++ frontend/src/app/admin/interesting-point/new-interesting-point/new-interesting-point.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {ConditionType, Place, PlacesType} from '../../../models/places';
+import {InterestingPointService} from '../../services/interesting-point.service';
 
 @Component({
   selector: 'app-new-interesting-point',
@@ -7,21 +12,63 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./new-interesting-point.component.scss']
 })
 export class NewInterestingPointComponent implements OnInit {
-
-  constructor() { }
+  conditionType = ConditionType;
+  placesType = PlacesType;
+  constructor(
+    private interestingPointService: InterestingPointService,
+    private router: Router,
+    private toastr: ToastrService,
+    public dialogRef: MatDialogRef<any>,
+    @Inject(MAT_DIALOG_DATA) public dialogData: Place
+  ) {
+  }
   form = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     SHAPE_Leng: new FormControl('', [Validators.required, Validators.minLength(5)]),
     SHAPE_Area: new FormControl('', [Validators.required, Validators.minLength(5)]),
     street: new FormControl('', []),
     house_number: new FormControl('', [Validators.required, Validators.maxLength(10)]),
-    neighborhood: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    operator: new FormControl('', [Validators.required, Validators.maxLength(10)]),
-    handicapped: new FormControl('', []),
-    condition: new FormControl('', [Validators.required, Validators.maxLength(6)]),
-    type: new FormControl('', [Validators.required, Validators.maxLength(6)]),
+
   });
   mode: any;
+  get name() {
+    return this.form.get('name');
+  }
+
+  get SHAPE_Leng() {
+    return this.form.get('SHAPE_Leng');
+  }
+
+  get SHAPE_Area() {
+    return this.form.get('SHAPE_Area');
+  }
+
+  get street() {
+    return this.form.get('street');
+  }
+
+  get house_number() {
+    return this.form.get('house_number');
+  }
+
+  get neighborhood() {
+    return this.form.get('neighborhood');
+  }
+
+  get operator() {
+    return this.form.get('operator');
+  }
+
+  get handicapped() {
+    return this.form.get('handicapped');
+  }
+
+  get condition() {
+    return this.form.get('condition');
+  }
+  get type(){
+    return this.form.get('type');
+  }
   ngOnInit(): void {
   }
 
