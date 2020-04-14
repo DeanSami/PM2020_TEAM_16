e0b3c1e79e7edfecc59268047f81db4091e81b80
eden ohana <edenoh2@ac.sce.ac.il>
2020-04-14 17:53:39 +0300

    interesting point ts

--- frontend/src/app/admin/interesting-point/interesting-point.component.ts
+++ frontend/src/app/admin/interesting-point/interesting-point.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ConditionType, ConditionTypeTitles, Place} from '../../models/places';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-interesting-point',
@@ -6,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interesting-point.component.scss']
 })
 export class InterestingPointComponent implements OnInit {
-
+  conditionType = ConditionType;
+  conditionTypeTitle = ConditionTypeTitles;
+  displayedColumns: string[] = ['name', 'street', 'neighborhood', 'operator', 'handicapped', 'condition', 'action'];
+  dataSource: MatTableDataSource<Place>;
+  places: Place[];
   constructor() { }
 
   ngOnInit(): void {
