244f0b54ef4739de3f8deeeb0e008940558d3a0a
eden ohana <edenoh2@ac.sce.ac.il>
2020-04-16 13:23:29 +0300

    interesting point ts

--- frontend/src/app/admin/interesting-point/interesting-point.component.ts
+++ frontend/src/app/admin/interesting-point/interesting-point.component.ts
@@ -5,6 +5,9 @@ import {ActivatedRoute} from '@angular/router';
 import {MatDialog} from '@angular/material/dialog';
 import {ToastrService} from 'ngx-toastr';
 import {InterestingPointService} from '../services/interesting-point.service';
+import {AreYouSureDialogComponent} from '../../are-you-sure-dialog/are-you-sure-dialog.component';
+import {NewDogParkComponent} from '../dog-parks/new-dog-park/new-dog-park.component';
+import {NewInterestingPointComponent} from './new-interesting-point/new-interesting-point.component';
 
 @Component({
   selector: 'app-interesting-point',
@@ -28,6 +31,38 @@ export class InterestingPointComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.places = this.rout.snapshot.data.InterestingPoint.places;
+    this.dataSource = new MatTableDataSource<Place>(this.places);
   }
 
+  removeInterestingPoint(interestingPointId: number) {
+    this.dialog.open(AreYouSureDialogComponent, {
+      width: '250px',
+    }).afterClosed().subscribe(result => {
+      if (result) {
+        this.interestingPointService.deleteInterestingPoint(interestingPointId).subscribe(res => {
+          console.log(res);
+          this.dataSource.data = this.dataSource.data.filter(park => park.id !== interestingPointId);
+          this.toastr.success('נמחק בהצלחה');
+        }, err =>  {
+          console.log(err);
+          this.toastr.error('ארעה שגיאה במחיקה');
+        });
+      }
+    });
+  }
+  editInterestingPoint(interestingPoint: Place) {
+    this.dialog.open(NewInterestingPointComponent, {
+      width: '600px',
+      data: interestingPoint
+    }).afterClosed().subscribe(result => {
+      if (result && result.status) {
+        const idx = this.dataSource.data.findIndex(park => park.id === result.places[0]);
+        if (idx >= 0) {
+          this.dataSource.data[idx] = result.places[0];
+        }
+        this.dataSource.data = this.dataSource.data;
+      }
+    });
+  }
 }
