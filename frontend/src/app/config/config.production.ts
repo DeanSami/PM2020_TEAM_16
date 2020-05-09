40b3ca9e3e005ca226c46fdeef8f52b2575f190e
edenoh2@ac.sce.ac.il <dorsh3@users.noreply.github.com>
2020-05-09 20:51:36 +0300

    u

--- frontend/src/app/config/config.production.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-  accessKeyId: 'AKIAYNRPTGKURKWH753F',
-  secretAccessKey: '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',
-  region: 'eu-west-1',
-  bucketARN: 'arn:aws:s3:::files.doggiehunt',
-  bucketName: 'files.doggiehunt'
-};
