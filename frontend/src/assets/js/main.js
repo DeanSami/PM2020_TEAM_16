1fd9522bd24c640891a8203c02b55550a7fdcdb9
edenoh2@ac.sce.ac.il <dorsh3@users.noreply.github.com>
2020-05-14 19:02:17 +0300

    config

--- frontend/src/assets/js/main.js
+++ frontend/src/assets/js/main.js
@@ -86,21 +86,6 @@
     $(".mobile-nav, .mobile-nav-toggle").hide();
   }
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1500, 'easeInOutExpo');
-    return false;
-  });
-
   // Initiate venobox lightbox
   $(document).ready(function() {
     $('.venobox').venobox();
